e53e9347f52ad04f6002b4b00ab726296d2f8a67
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-11-23 02:06:47 +0200

    preventing an attacker from hijacking the system through our script

--- Mac Address Changer/mac_changer.py
+++ Mac Address Changer/mac_changer.py
@@ -12,8 +12,6 @@ import subprocess
 interface = input("interface > ")
 new_mac = input("mac > ")
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up ", shell=True)
-
-
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
