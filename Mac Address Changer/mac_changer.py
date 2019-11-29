c1b3bafebfaad2daa7eebd82d85fb8c22234139f
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-11-29 23:21:03 +0200

    added better error handling

--- Mac Address Changer/mac_changer.py
+++ Mac Address Changer/mac_changer.py
@@ -14,16 +14,9 @@ def change_mac(interface, new_mac):
     # this way of calling the call method is more secure so that a hacker can't hijack the system
     # by using for example: ;ls; because python will treat this whole list as a single command and not
     # as multiple commands
-    if interface is None and new_mac is None:
-        print("no interface and mac address specified, exiting...")
-    elif interface is None:
-        print("no interface specified, exiting...")
-    elif new_mac is None:
-        print("no mac address specified, exiting...")
-    else:
-        subprocess.call(["ifconfig", interface, "down"])
-        subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-        subprocess.call(["ifconfig", interface, "up"])
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
 def parse_command_line():
     # creating an instance from OptionParser
@@ -34,9 +27,26 @@ def parse_command_line():
     parser.add_option("-m", "--mac", dest="new_mac", help="The new MAC address of the interface chosen")
     # parsing the arguments passed to add_option method above and 
     # returned its return value (which is a tuple of the Values and list of the arguments)
-    return parser.parse_args()
+    values = parser.parse_args()
+    err =0
+    if values[0].interface is None and values[0].new_mac is None:
+        print("no interface and mac address specified, exiting...")
+        err=2
+    elif values[0].interface is None:
+        print("no interface specified, exiting...")
+        err=1
+    elif values[0].new_mac is None:
+        print("no mac address specified, exiting...")
+        err=1
+    return (parser.parse_args(),err)
 
 values = parse_command_line()
-change_mac(values[0].interface, values[0].new_mac)
+if values[1]==0:
+    change_mac(values[0][0].interface, values[0][0].new_mac)
+else:
+    print("errors: ", values[1])
+
+#result = subprocess.check_output(["ifconfig", values[0].interface])
+#print(result)
 
 
