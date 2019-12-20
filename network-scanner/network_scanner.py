a659a609484b65dd221c5f0f6e3f82d53fb60d92
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-12-20 15:59:53 +0200

    changed the shebang to python 2

--- network-scanner/network_scanner.py
+++ network-scanner/network_scanner.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Thu Dec 19 23:39:34 2019
@@ -10,4 +10,6 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_req = scapy.ARP()
-    print(arp_req.summary)
+    print(arp_req.summary())
+    
+scan("192.168.1.1/24")
