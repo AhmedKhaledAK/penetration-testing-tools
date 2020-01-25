09c9c488e614cf860b6728f0d393620e3d74e649
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-01-25 23:27:46 +0200

    changed variable names

--- network-scanner/network_scanner.py
+++ network-scanner/network_scanner.py
@@ -9,13 +9,13 @@ Created on Thu Dec 19 23:39:34 2019
 import scapy.all as scapy
 
 def scan(ip):
-    arp_req = scapy.ARP()
-    arp_req.pdst=ip
-    arp_req.show()
-    broadcast = scapy.Ether()
-    broadcast.dst = "ff:ff:ff:ff:ff:ff"
-    broadcast.show()
-    bc_arpreq = broadcast/arp_req
-    bc_arpreq.show()
+    arp_req_packet = scapy.ARP()
+    arp_req_packet.pdst=ip
+    arp_req_packet.show()
+    broadcast_packet = scapy.Ether()
+    broadcast_packet.dst = "ff:ff:ff:ff:ff:ff"
+    broadcast_packet.show()
+    bc_arpreq_packet = broadcast_packet/arp_req_packet
+    bc_arpreq_packet.show()
     
 scan("<ip>")
