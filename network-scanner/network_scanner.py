d141890fe1df524021982b52679cb5d850c41857
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-01-26 20:46:37 +0200

    removed unnecessary lines

--- network-scanner/network_scanner.py
+++ network-scanner/network_scanner.py
@@ -21,14 +21,11 @@ def scan(ip):
     # the next two lines creates a packet that asks for a specific ip
     arp_req_packet = scapy.ARP()
     arp_req_packet.pdst=ip
-    arp_req_packet.show()
     # creating the broadcast MAC address packet
     broadcast_packet = scapy.Ether()
     broadcast_packet.dst = "ff:ff:ff:ff:ff:ff"
-    broadcast_packet.show()
     # combining packets
     bc_arpreq_packet = broadcast_packet/arp_req_packet
-    bc_arpreq_packet.show()
     # sending and receiving packets, the answered ones and the unanswered. The timeout is necessary.  
     ans_list = scapy.srp(bc_arpreq_packet, timeout=1)[0]
     for el in ans_list:
