34e9c9557ec642e3311a2fff5c2b141ff1644368
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-06-25 22:12:32 +0200

    parse_args fn

--- netcat-replacement/net_tool.py
+++ netcat-replacement/net_tool.py
@@ -26,3 +26,34 @@ def usage():
     print("net_tool.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\"")
     print("echo 'ABCDEFGHI' | ./net_tool.py -t 192.168.11.12 -p 135")
     sys.exit(0)
+
+def parse_args():
+    if len(sys.argv[1:]) == 0:
+        return None, None
+
+    opts, args = None, None
+    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hle:t:p:cu:", ["help","listen","execute","target","port","command","upload"])
+    except getopt.GetoptError as err:
+        print(str(err))
+        opts, args = None, None
+    
+    #print("opts:",opts)
+    #print("args:",args)
+
+    return opts, args
+
+
+def main():
+    global listen
+    global shell_cmd
+    global upload 
+    global execute_cmd
+    global target
+    global upload_dest
+    global port
+
+    parse_args()
+
+main()
