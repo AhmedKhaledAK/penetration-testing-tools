b1115cb3e223352a32ad636364e0aec25d050a60
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-06-29 23:52:01 +0200

    starter

--- pcap-processor/pcap_processor.py
+++ pcap-processor/pcap_processor.py
@@ -3,4 +3,36 @@
 """
 
 """
+import re
+import zlib
+import scapy.all as scapy
 
+images_dir = "/home/Documets/Kali-Linux/penetration-testing-tools/pcap-processor/images"
+pcap_file = "arp_spoofer.pcap"
+
+def find_http(pcap_file):
+    images = 0
+    packets = scapy.rdpcap(pcap_file)
+    sessions = packets.sessions()
+    var = 0
+    for sess in sessions:
+        payload = ""
+        for packet in sessions[sess]:
+            try:
+                if 'TCP' in packet:
+                    print("TCP FOUND")
+                    var += 1
+                    #print(packet['TCP'])
+                if packet['TCP'].dport == 80 or packet['TCP'].sport == 80:
+                    payload += str(packet['TCP'].payload)
+            except:
+                pass
+            
+            
+    print("var:",var)
+
+
+def main():
+    find_http(pcap_file)
+
+main()
