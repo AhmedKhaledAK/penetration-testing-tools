5c6b248e6dfe97a716c81c636a0a89ec42eccf07
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-07-05 19:41:13 +0200

    starting code

--- web-mapping/web_mapper.py
+++ web-mapping/web_mapper.py
@@ -3,4 +3,23 @@
 """
 
 """
+import urllib as crawler
+import queue
+import _thread
+import os
 
+threads = 10
+
+target = "https:/www.example.com" # your target website here
+directory = "/home/ahmedkhaled/Downloads/Wordpress/latest/wordpress"
+
+os.chdir(directory)
+
+paths = queue.Queue()
+
+for root, dirs, files in os.walk("."):
+    for file in files:
+        path = "%s/%s" % (root, file)
+        if path.startswith("."):
+            path = path[1:]
+        paths.put(path)
