cdc508cdaaab72723f5c3fea4e8093e4ac525f95
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-07-05 21:48:11 +0200

    fixing bugs

--- web-mapping/web_mapper.py
+++ web-mapping/web_mapper.py
@@ -3,13 +3,14 @@
 """
 
 """
-import urllib as crawler
+import urllib.request as crawler
+import urllib.error as cerr
 import queue
-import thread
+import threading
 import os
 
 threads = 10
-        
+cnt = 0
 def get_paths():
     paths = queue.Queue()
     for root, dirs, files in os.walk("."):
@@ -22,12 +23,17 @@ def get_paths():
 
 
 def test_path(paths, target):
+    global cnt
     while not paths.empty():
         path = paths.get()
         url = "%s%s" % (target, path)
-        req = crawler.request.Request(url)
+        print("here1")
+        req = crawler.Request(url)
+        print("req:", req.get_full_url())
         try:
-            response = crawler.request.urlopen(req)
+            print("here")
+            response = crawler.urlopen(req)
+            print("here2")
             content = response.read()
             
             print("response content:")
@@ -35,12 +41,13 @@ def test_path(paths, target):
             print("response code:", response.code)
             print("response path:", path)
             response.close()
-        except crawler.request.HTTPError as err:
-            print("failed crawling; error code:", err.code)
+        except cerr.HTTPError as err:
+            cnt += 1
+            print("failed crawling; error code:")
             pass
 
 def main():
-    target = "https:/www.example.com" # your target website here
+    target = "https://www.example.com" # your target website here
     directory = "/home/ahmedkhaled/Downloads/Wordpress/latest/wordpress"
     
     os.chdir(directory)
@@ -49,6 +56,8 @@ def main():
     
     for i in range(threads):
         print("thread #:", i)
-        thread.start_new_thread(test_path, (paths, target))     
+        t = threading.Thread(target=test_path, args=(paths, target))
+        t.start()     
     
+    print(cnt)
 main()
